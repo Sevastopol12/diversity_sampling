d44a1bb8287dda389cb2dc75ae5e63a89050fa2d
Sevastopol12 <chkalov253@gmail.com>
2026-04-21 18:56:09 +0700

    fix(database): add modules

--- diversity_sampling/database/__init__.py
+++ diversity_sampling/database/__init__.py
@@ -1,4 +1,3 @@
-from .api import fetch_data
+from .api import fetch_data, get_augment_set, get_retain_set, get_prune_set
 
-__all__ = ["fetch_data"]
-
+__all__ = ["fetch_data", "get_augment_set", "get_retain_set", "get_prune_set"]
--- diversity_sampling/database/api/__init__.py
+++ diversity_sampling/database/api/__init__.py
@@ -1,3 +1,3 @@
-from .select import fetch_data
+from .select import fetch_data, get_augment_set, get_retain_set, get_prune_set
 
-__all__ = ["fetch_data"]
+__all__ = ["fetch_data", "get_augment_set", "get_retain_set", "get_prune_set"]
